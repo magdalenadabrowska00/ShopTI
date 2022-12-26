f3309fc7269ef26ea33f13351d847368747b224c
magda <maddabrowska@gmail.com>
2022-12-26 14:16:38 +0100

    add entities

---
+++ Entities/OrderDetail.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopTI.Entities
+{
+    public class OrderDetail
+    {
+        public int OrderDetailId { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int OrderId { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopTI.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public decimal Price { get; set; }
+        public string ProductName { get; set; }
+    }
+}
