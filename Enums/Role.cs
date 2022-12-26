7736754dc062d028b91a064624bcb2da28655759
magda <maddabrowska@gmail.com>
2022-12-26 19:03:49 +0100

    add user registration endpoint

---
+++ Enums/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace ShopTI.Enums
+{
+    public enum Role
+    {
+        Admin = 0,
+        User = 1
+    }
+}
---
+++ IServices/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using ShopTI.Models;
+
+namespace ShopTI.IServices
+{
+    public interface IAccountService
+    {
+        void RegisterUser(RegisterUser newUser);
+        
+
+        
+    }
+}
